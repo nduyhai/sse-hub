99bd93916804024f5301d345986411e6aaa2a9cf
nduyhai <nduyhai@users.noreply.github.com>
2026-05-11 13:31:38 +0700

    feat: init project

---
+++ src/main/java/com/nduyhai/ssehub/SseHubApplication.java
@@ -0,0 +1,13 @@
+package com.nduyhai.ssehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseHubApplication.class, args);
+    }
+
+}
